c5fc2b7156b71bea1c7ea585ee1dfa41ab1c2a95
bruno spies <bruno.spies@ecomp.ufsm.br>
2024-12-07 03:09:25 -0300

    updating shift unit

--- RV32I/rtl/shift_unit.vhd
+++ RV32I/rtl/shift_unit.vhd
@@ -17,7 +17,7 @@ end shift_unit;
 
 architecture behavioral of shift_unit is
 
-    type array_shift is array (31 downto 0) of std_logic_vector(31 downto 0);
+    type array_shift is array (32 downto 0) of std_logic_vector(31 downto 0);
     
     signal results_sll: array_shift;
     signal results_srl: array_shift;
@@ -32,17 +32,21 @@ begin
         results_sra(i) <= ((others=>operand1(31)) & operand1(31 downto i));
     end generate SHIFT_GENERATE;
 
-    results_sll(31) <= (operand1(0) & (others=>'0'));
-    results_srl(31) <= ((others=>'0') & operand(31));
+    results_sll(31) <= operand1(31) & (others=>'0');
+    results_srl(31) <= (others=>'0') & operand1(31);
     results_sra(31) <= (others=>operand1(31));
 
-    
-        
+    results_sll(32) <= (others=>'0');
+    results_srl(32) <= (others=>'0');
+    results_sra(32) <= (others=>'0');
 
+    shift_amount <= to_integer(to_unsigned(operand2)) when operand2 < x"00000010" else -- operand2<32
+                    32;
 
-std_logic_vector(op1_u sll to_integer(op2_u))                  when operation = SLLI or operation = SLLL else
-std_logic_vector(op1_u srl to_integer(op2_u))                  when operation = SRLI or operation = SRLL else
-std_logic_vector(shift_right_signed(op1_s, to_integer(op2_u))) when operation = SRAI or operation = SRAA else
+    result <= results_sll(shift_amount) when operation = SLLI or operation = SLLL else
+              results_srl(shift_amount) when operation = SRLI or operation = SRLL else
+              results_sra(shift_amount) when operation = SRAI or operation = SRAA else
+              (others=>'0');
               
 end behavioral;
 

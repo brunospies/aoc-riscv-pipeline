93dfbcd89e62093088be03da8b9bb7a9c63770e4
desenvolvimento <bruno.spies@ecomp.ufsm.br>
2024-12-12 15:34:28 -0300

    fixing Imediate Generator Problems

--- RV32I/rtl/ImmediateDataExtractor.vhd
+++ RV32I/rtl/ImmediateDataExtractor.vhd
@@ -17,29 +17,19 @@ entity ImmediateDataExtractor is
     );
 end ImmediateDataExtractor;
 
---architecture arch1 of ImmediateDataExtractor is
-
---begin
---    imm_data <= (others=>instruction(31)) & instruction(31 downto 20) when instruction_f = I else
---                (others=>instruction(31)) & instruction(31 downto 25) & instruction(11 downto 7) when instruction_f = S else
---                (others=>instruction(31)) & instruction(7) & instruction(30 downto 25) & instruction(11 downto 8) & '0' when instruction_f = B else
---                instructionothers & (others=>'0') when instruction_f = U else
---                (others=>instruction(31)) & instruction(31) & instruction(19 downto 12) & instruction(20) & instruction(30 downto 21) & '0';
---end arch1;
-
 architecture arch2 of ImmediateDataExtractor is
     begin
         imm_data <= 
             -- Tipo I: Load e instruções imediatas
-            (instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31 downto 20)) when instruction_f = I else
+            ((19 downto 0 =>instruction(31)) & instruction(31 downto 20)) when instruction_f = I else
             -- Tipo S: Store
-            (instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31 downto 25) & instruction(11 downto 7)) when instruction_f = S else
+            ((19 downto 0 =>instruction(31)) & instruction(31 downto 25) & instruction(11 downto 7)) when instruction_f = S else
             -- Tipo B: Branch
-            (instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(7) & instruction(30 downto 25) & instruction(11 downto 8) & '0') when instruction_f = B else
+            ((19 downto 0 =>instruction(31)) & instruction(7) & instruction(30 downto 25) & instruction(11 downto 8) & '0') when instruction_f = B else
             -- Tipo U: LUI e AUIPC
             (instruction(31 downto 12) & "000000000000") when instruction_f = U else
             -- Tipo J: Jump
-            (instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(31) & instruction(19 downto 12) & instruction(20) & instruction(30 downto 21) & "0");
+            ((11 downto 0 =>instruction(31)) & instruction(19 downto 12) & instruction(20) & instruction(30 downto 21) & "0");
 
     end arch2;
     
